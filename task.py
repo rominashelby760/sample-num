636284fef4357c498c871edd435bacb2b9259c1d
rominashelby760 <138257422+rominashelby760@users.noreply.github.com>
2023-07-03 22:29:18 +0200

    Create task.py

---
+++ task.py
@@ -0,0 +1,15 @@
+def find_primes(start, end):
+    primes = []
+    for num in range(start, end + 1):
+        if num > 1:
+            for i in range(2, int(num**0.5) + 1):
+                if num % i == 0:
+                    break
+            else:
+                primes.append(num)
+    return primes
+
+start = 1
+end = 100
+prime_numbers = find_primes(start, end)
+print("Простые числа в диапазоне от", start, "до", end, ":", prime_numbers)
